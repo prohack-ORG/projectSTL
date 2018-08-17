1e60fc51ae94a3bf1da19a7751aecdc0a6ef2751
Raman Jaiswal <vipram91@gmail.com>
2018-08-17 21:19:46 +0530

    moving Build script to prject root

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #set -x
 is_clean=$1
 REPO_DIR=$(git rev-parse --show-toplevel)
-CURR_DIR=${REPO_DIR}/build
+BUILD_DIR=${REPO_DIR}/build
 DEBUG_FLAG=b #a
-rm -rf ${CURR_DIR}/bin/
-rm -rf ${CURR_DIR}/lib/
 
 if ${is_clean};
 then
-    rm -rf ${CURR_DIR}/builds/*
+    rm -rf ${BUILD_DIR}/bin/
+    rm -rf ${BUILD_DIR}/lib/
+    rm -rf ${BUILD_DIR}/builds/*
 fi
 
-mkdir -p ${CURR_DIR}/builds
+mkdir -p ${BUILD_DIR}/builds
 
-pushd ${CURR_DIR}/builds/
+pushd ${BUILD_DIR}/builds/
 cmake ../../ -DCMAKE_BUILD_TYPE=Debug
 make clean
-make -j2 --debug=${DEBUG_FLAG}
+make -j2 #--debug=${DEBUG_FLAG}
 popd
 
 
